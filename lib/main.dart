aacf90f92ecec674eb17db6eaa154c4a0fb9b96d
Eduardo Chavez <eduardochavezv@gmail.com>
2022-09-29 19:12:18 -0500

    metodos para sonido y teclas

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,43 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   AudioCache audioCache = AudioCache();
+
+  void playNote(int note) {
+    audioCache.play("audios/note$note.wav");
+  }
+
+  Widget buildKey({required Color color, required int note}) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: () {
+            playNote(note);
+          },
+          child: Text(""),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            audioCache.play("audios/note1.wav");
-          },
-          child: Text("Click"),
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("Xylophone"),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // buildKey(Colors.indigo, 1),
+          // buildKey(Colors.red, 2),
+          // buildKey(Colors.amber, 3),
+          buildKey(color: Colors.pink, note: 1),
+          buildKey(color: Colors.blue, note: 2),
+          buildKey(color: Colors.yellow, note: 3),
+        ],
       ),
     );
   }
